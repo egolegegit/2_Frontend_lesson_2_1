f2bfc5edf474f61076e4a443e367b61c7675081c
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-09-20 23:58:17 +0700

    use hook useHistory

--- src/app/components/userPage.jsx
+++ src/app/components/userPage.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import api from '../api'
 import Loader from './loader'
 import Qualitie from './qualitie'
 
 const UserPage = () => {
   const params = useParams()
+  const history = useHistory()
   const { userId } = params
   const [user, setUser] = useState()
 
@@ -24,11 +25,11 @@ const UserPage = () => {
 
   if (user) {
     return (
-      <div className="d-flex flex-column p-4 vw-100 vh-100">
-        <div className="card border-info mb-3 w-50">
+      <div className="p-4 d-flex flex-column vw-100 vh-100">
+        <div className="mb-3 card border-info w-50">
           <div className="card-body">
             <h2 className="card-title">{user.name}</h2>
-            <h5 className="card-subtitle mb-2 text-muted">
+            <h5 className="mb-2 card-subtitle text-muted">
               Качества:
               {user.qualities.map((el, idx) => (
                 <Qualitie key={idx} qualitie={el} />
@@ -37,9 +38,15 @@ const UserPage = () => {
             <h5>{`Профессия: ${user.profession.name}`}</h5>
             <h5>{`Кол-во встреч: ${user.completedMeetings}`}</h5>
             <h5>{`Оценка: ${user.rate}`}</h5>
-            <Link className="mt-2 btn btn-secondary w-30" to="/users">
+            {/* <Link className="mt-2 btn btn-secondary w-30" to="/users">
               Все пользователи
-            </Link>
+            </Link> */}
+            <button
+              className="mt-2 btn btn-secondary w-30"
+              onClick={() => history.push('/users')}
+            >
+              Все пользователи
+            </button>
           </div>
         </div>
       </div>
