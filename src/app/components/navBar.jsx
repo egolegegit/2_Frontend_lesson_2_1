7c457316b52c33dad0a736d4c06eb200740bcfa6
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-09-20 14:21:30 +0700

    fix style Link component

--- src/app/components/navBar.jsx
+++ src/app/components/navBar.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div className="nav ps-2">
-      <Link to="/" className="nav-item nav-link">
+    <div className="nav ps-2 pt-1">
+      <NavLink
+        exact
+        to="/"
+        className="nav-item nav-link"
+        activeClassName="bg-secondary bg-opacity-25"
+      >
         Main
-      </Link>
-      <Link to="/login" className="nav-item nav-link">
+      </NavLink>
+      <NavLink
+        to="/login"
+        className="nav-item nav-link"
+        activeClassName="bg-secondary bg-opacity-25"
+      >
         Login
-      </Link>
-      <Link to="/users" className="nav-item nav-link">
+      </NavLink>
+      <NavLink
+        to="/users"
+        className="nav-item nav-link"
+        activeClassName="bg-secondary bg-opacity-25"
+      >
         Users
-      </Link>
+      </NavLink>
     </div>
   )
 }
