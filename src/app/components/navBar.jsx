410fb44c9ff551efe4c0c53a7319f3d0ea6b7720
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-09-27 16:10:59 +0700

    add tailwindcss + toggletheme component

--- src/app/components/navBar.jsx
+++ src/app/components/navBar.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import ThemeToggle from './themetoggle/ThemeToggle'
 
 const NavBar = () => {
   return (
-    <div className="nav ps-2 pt-1">
+    <div className="nav pe-4 pt-1 justify-end align-items-center">
+      <ThemeToggle />
       <NavLink
         exact
         to="/"
-        className="nav-item nav-link"
+        className="nav-item nav-link rounded-2"
         activeClassName="bg-secondary bg-opacity-25"
       >
         Main
       </NavLink>
       <NavLink
         to="/login"
-        className="nav-item nav-link"
+        className="nav-item nav-link rounded-2"
         activeClassName="bg-secondary bg-opacity-25"
       >
         Login
       </NavLink>
       <NavLink
         to="/users"
-        className="nav-item nav-link"
+        className="nav-item nav-link rounded-2"
         activeClassName="bg-secondary bg-opacity-25"
       >
         Users
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import App from './app/App'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import App from './app/App'
 
 ReactDOM.render(
   <React.StrictMode>
