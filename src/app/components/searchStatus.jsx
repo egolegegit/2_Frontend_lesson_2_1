bb86b38244b8a4ca793ad93b14e56e7e87c44d9a
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-10-03 01:04:11 +0700

    fix style component

--- src/app/components/searchStatus.jsx
+++ src/app/components/searchStatus.jsx
@@ -5,16 +5,17 @@ import PropTypes from 'prop-types'
 const SearchStatus = ({ count }) => {
   const titles = ['человек', 'человека', 'человек']
 
-  let classesTitle = 'badge bg-'
-  classesTitle += count !== 0 ? 'primary' : 'danger'
+  let classesTitle =
+    'rounded border-transparent text-white py-1 px-2 flex align-center whitespace-nowrap'
+  classesTitle += count !== 0 ? ' bg-blue-600' : ' bg-red-600'
   const texTitle =
     count !== 0
       ? `${count} ${renderPhrase(count, titles)} тусанется сегодня с тобой `
       : 'Никто с тобой не тусанет'
   return (
-    <h4>
-      <span className={`badge bg-${classesTitle}`}>{texTitle}</span>
-    </h4>
+    <h5>
+      <span className={`${classesTitle}`}>{texTitle}</span>
+    </h5>
   )
 }
 
