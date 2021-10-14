92f6bd21e540db6ca7f2db22c60864bfd4cada57
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-10-15 01:37:09 +0700

    add form registraion

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -1,6 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { validator } from '../../utils/validator'
+import TextField from '../common/form/textField'
+
 const RegisterForm = () => {
-  return <h1>Register form</h1>
+  const [data, setData] = useState({ email: '', password: '' })
+  const [errors, setErrors] = useState({})
+
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({ ...prevState, [target.name]: target.value }))
+  }
+
+  const validatorConfig = {
+    email: {
+      isRequired: { message: '* Please enter a valid email address' },
+      isEmail: { message: 'Email address entered incorrectly' },
+    },
+    password: {
+      isRequired: { message: '* Please enter a valid password' },
+      isCapitalSymbol: { message: 'Password must contain 1 capital letter' },
+      isContainDigit: { message: 'password must contain at least 1 number' },
+      isMin: {
+        message: 'password must contain min 8 simbol',
+        value: 8,
+      },
+    },
+  }
+
+  useEffect(() => {
+    validate()
+  }, [data])
+
+  const validate = () => {
+    const errors = validator(data, validatorConfig)
+
+    setErrors(errors)
+    return Object.keys(errors).length === 0
+  }
+
+  const isValid = Object.keys(errors).length === 0
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const isValid = validate()
+
+    if (!isValid) return
+    console.log(errors, data)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center w-full text-center"
+    >
+      <TextField
+        label="Email"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+        error={errors.email}
+      />
+      <TextField
+        label="password"
+        name="password"
+        type={'password'}
+        value={data.password}
+        onChange={handleChange}
+        error={errors.password}
+      />
+      <button
+        disabled={!isValid}
+        className={` 
+            bg-gray-400 mt-8 border rounded-xl border-transparent p-2 hover:text-white w-3/4 text-white dark:bg-gray-600 ${
+              isValid ? 'hover:bg-blue-700 text-white animate-pulse' : ''
+            } 
+          `}
+      >
+        Submit
+      </button>
+    </form>
+  )
 }
 
 export default RegisterForm
--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -16,26 +16,27 @@ const Login = () => {
   return (
     <div className="pt-40 layout-content">
       <div className="flex flex-col items-center w-full rounded shadow p-14 wrapper-form sm:w-2/3 md:w-2/5 lg:w-2/6 dark:border-green-400 border-1">
-        <h2>Login</h2>
         {formType === 'register' ? (
           <>
+            <h2>Register</h2>
+            <RegisterForm />{' '}
             <span>
               Already have account?
               <a role="button" onClick={toggleFormType}>
                 Sign In
               </a>
             </span>
-            <RegisterForm />
           </>
         ) : (
           <>
+            <h2>Login</h2>
+            <LoginForm />
             <span>
               Dont have account?
               <a role="button" onClick={toggleFormType}>
                 Sign UP
               </a>
             </span>
-            <LoginForm />
           </>
         )}
       </div>
