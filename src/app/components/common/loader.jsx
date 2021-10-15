a536094c13d74a1d937bcf2c54def532a6df6cb7
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-10-16 00:25:55 +0700

    fix loader component

--- src/app/components/common/loader.jsx
+++ src/app/components/common/loader.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 const Loader = () => {
   return (
-    <div className="d-flex align-items-center justify-content-center layout-content">
-      <span className="fs-2 fw-bold pe-4 dark:text-white">Loading...</span>
-      <div
-        className="spinner-border dark:text-white"
-        style={{ width: '2rem', height: '2rem' }}
-        role="status"
-      ></div>
+    <div className="w-full d-flex align-items-center justify-content-center layout-content">
+      <div className="w-full d-flex align-items-center justify-content-center">
+        <span className="fs-2 fw-bold pe-4 dark:text-white">Loading...</span>
+        <div
+          className="spinner-border dark:text-white"
+          style={{ width: '2rem', height: '2rem' }}
+          role="status"
+        ></div>
+      </div>
     </div>
   )
 }
