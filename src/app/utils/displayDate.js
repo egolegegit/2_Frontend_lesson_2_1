3ee2b3ab424bcb7043ea316bf06301d95d148326
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-10-20 10:04:09 +0700

    output date comment

--- src/app/utils/displayDate.js
+++ src/app/utils/displayDate.js
@@ -1,30 +1,31 @@
-import renderPhrase  from './renderPhrase'
+import renderPhrase from './renderPhrase'
 
 const displayDate = (data) => {
   let displaydate = ''
-  const dateNow = new Date()
+  const dateNow = Date.now()
   const diffData = dateNow - data
-  // console.log('dateNow', dateNow, 'data', data)
-  console.log('diffData', diffData)
   const timeScale = [
     { title: '1 минуту назад', rangeBefore: 0, rangeAfter: 60000 },
     { title: '5 минут назад', rangeBefore: 60000, rangeAfter: 300000 },
     { title: '10 минут назад', rangeBefore: 300000, rangeAfter: 600000 },
     { title: '30 минут назад', rangeBefore: 600000, rangeAfter: 1800000 },
     { title: 'hours.minutes', rangeBefore: 1800000, rangeAfter: 3600000 },
-    { title: 'day.month', rangeBefore: 3600000, rangeAfter: 86400000 },
-    { title: 'day.month.year', rangeBefore: 86400000, rangeAfter: 2592000000 },
+    { title: 'day.month', rangeBefore: 86400000, rangeAfter: 2592000000 },
+    {
+      title: 'day.month.year',
+      rangeBefore: 2592000000,
+      rangeAfter: 2592000000000000,
+    },
   ]
 
-    const yearsTitles = ['год', 'года', 'лет']
-    const monthTitles = ['месяц', 'месяца', 'месяцев']
-    const dayTitles = ['день', 'дня', 'дней']
-    const hoursTitles = ['час', 'часа', 'часов']
-    const minutesTitles = ['минута', 'минуты', 'минут']
-
+  const yearsTitles = ['год', 'года', 'лет']
+  const monthTitles = ['месяц', 'месяца', 'месяцев']
+  const dayTitles = ['день', 'дня', 'дней']
+  const hoursTitles = ['час', 'часа', 'часов']
+  const minutesTitles = ['минута', 'минуты', 'минут']
 
   const isRange = (data, rangeBefore, rangeAfter) => {
-    if (data >= rangeAfter && data <= rangeBefore) return true
+    if (data > rangeAfter && data < rangeBefore) return true
     return false
   }
 
@@ -39,8 +40,6 @@ const displayDate = (data) => {
       }
     }
   }
-
-  console.log(result)
 
   switch (result.title) {
     case '1 минуту назад':
@@ -60,19 +59,58 @@ const displayDate = (data) => {
       break
 
     case 'hours.minutes':
-      displaydate = Math.trunc(diffData / result.rangeAfter)
+      {
+        const hours = diffData / result.rangeBefore
+        const hoursTrunc = Math.trunc(hours)
+        const minutes = Math.trunc(
+          (diffData % result.rangeBefore) / result.rangeBefore
+        )
+        if (hoursTrunc) {
+          displaydate = `${hoursTrunc} ${renderPhrase(hoursTrunc, hoursTitles)}`
+        }
+        if (minutes) {
+          displaydate =
+            displaydate + ` ${minutes} ${renderPhrase(minutes, minutesTitles)}`
+        }
+
+        displaydate = displaydate + ' назад'
+      }
       break
 
     case 'day.month':
-      displaydate = Math.trunc(diffData / result.rangeAfter)
+      {
+        const month = diffData / result.rangeBefore
+        const monthTrunc = Math.trunc(month)
+        const day = Math.trunc(
+          (diffData % result.rangeBefore) / result.rangeBefore
+        )
+        if (monthTrunc) {
+          displaydate = `${monthTrunc} ${renderPhrase(monthTrunc, monthTitles)}`
+        }
+        if (day) {
+          displaydate = displaydate + ` ${day} ${renderPhrase(day, dayTitles)}`
+        }
+
+        displaydate = displaydate + ' назад'
+      }
       break
 
     case 'day.month.year':
       {
-        console.log(result.rangeAfter)
-        const years = Math.trunc(diffData / result.rangeAfter)
-        const month = Math.trunc(diffData / result.rangeAfter)
-        displaydate = `${years} ${renderPhrase(years, yearsTitles)}`
+        const years = Math.trunc(diffData / result.rangeBefore)
+        const yearsTrunc = Math.trunc(years)
+        const month = Math.trunc(
+          (diffData % result.rangeBefore) / result.rangeBefore
+        )
+        if (yearsTrunc) {
+          displaydate = `${yearsTrunc} ${renderPhrase(yearsTrunc, yearsTitles)}`
+        }
+        if (month) {
+          displaydate =
+            displaydate + ` ${month} ${renderPhrase(month, monthTitles)}`
+        }
+
+        displaydate = displaydate + ' назад'
       }
       break
   }
