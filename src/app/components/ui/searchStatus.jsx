00b2a0f17941168b7a5da3501bf68ba8b456f7af
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2021-10-20 18:04:52 +0700

    fix styled searchStatus.jsx

--- src/app/components/ui/searchStatus.jsx
+++ src/app/components/ui/searchStatus.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import  renderPhrase  from '../../utils/renderPhrase'
+import renderPhrase from '../../utils/renderPhrase'
 import PropTypes from 'prop-types'
 
 const SearchStatus = ({ count }) => {
   const titles = ['человек', 'человека', 'человек']
 
   let classesTitle =
-    'rounded border-transparent text-white py-1 px-2 flex align-center whitespace-nowrap'
-  classesTitle += count !== 0 ? ' bg-blue-600' : ' bg-red-600'
+    'rounded border-transparent dark:text-white py-1 px-2 flex align-center whitespace-nowrap dark:bg-gray-600'
+  classesTitle += count !== 0 ? ' bg-gray-300' : ' bg-red-600 text-white'
   const texTitle =
     count !== 0
       ? `${count} ${renderPhrase(count, titles)} тусанется сегодня с тобой `
       : 'Никто с тобой не тусанет'
   return (
-    <h5>
+    <h5 className="ml-[7.5rem]">
       <span className={`${classesTitle}`}>{texTitle}</span>
     </h5>
   )
